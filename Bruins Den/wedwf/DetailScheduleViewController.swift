6c2d0fa95adc19e4ec41be4e59c1729b7d62714c
Siddarth Ijju <dulluijju@gmail.com>
2018-11-14 01:05:53 -0700

    Fixed previous commit. Updated schedules. Finals schedules needs fix

--- Bruins Den/wedwf/DetailScheduleViewController.swift
+++ Bruins Den/wedwf/DetailScheduleViewController.swift
@@ -12,28 +12,14 @@ class DetailScheduleViewController: UIViewController {
     var type = -1 //what bell schedule to display
     var container : BellScreenContainerViewController?
     
-    //@IBOutlet weak var imageView: UIImageView!
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        //self.imageView.translatesAutoresizingMaskIntoConstraints = true
-//        self.title = "Bell Schedule"
-//        if type == 0 {
-//            container?.imageview.image = #imageLiteral(resourceName: "regularSchoolDay")
-//            
-//            //imageView.image =
-//        } else if type == 1 {
-//            container?.imageview.image = #imageLiteral(resourceName: "stormDelay")
-//
-//
-//        } else if type == 2 {
-//            container?.imageview.image = #imageLiteral(resourceName: "assemblySchedule")
-//
-//        } else {
-//           container?.imageview.image = #imageLiteral(resourceName: "finalExams")
-//        }
-//
-//        // Do any additional setup after loading the view.
-//    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.imageView.translatesAutoresizingMaskIntoConstraints = true
+        self.title = "Bell Schedule"
+        // Do any additional setup after loading the view.
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -48,8 +34,9 @@ class DetailScheduleViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        if segue.identifier == "toImage" {
-            container = segue.destination as! BellScreenContainerViewController
+        if segue.identifier == "toSchedule" {
+            container = segue.destination as? BellScreenContainerViewController
+            container?.sc = type
         }
     }
     
--- bruins-den/Bruins Den/Pods/Target Support Files/Pods-Bruins Den/Pods-Bruins Den-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bruins_Den : NSObject
-@end
-@implementation PodsDummy_Pods_Bruins_Den
-@end
